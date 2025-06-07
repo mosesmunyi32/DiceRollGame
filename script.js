a9113ab3b1646fa71e6932e36d33c40cac6e3336
mosesmunyi <mosesmunyi31@gmail.com>
2025-06-07 12:32:14 +0300

    Display the winner and Game information

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-"use strict";
+'use strict';
 
 //select the elements that hold main scores for the players
-const score0El = document.querySelector("#score--0");
-const score1El = document.getElementById("score--1"); //faster when doing large selection
-const current0El = document.getElementById("current--0");
-const current1El = document.getElementById("current--1");
-const diceEl = document.querySelector(".dice"); //select the dice
-const player0El = document.querySelector(".player--0");
-const player1El = document.querySelector(".player--1");
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1'); //faster when doing large selection
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice'); //select the dice
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const winnerInformation = document.querySelector('.winner-info');
 
 //>> Event listeners for rolling the Dice
-const btnNew = document.querySelector(".btn--new");
-const btnRoll = document.querySelector(".btn--roll");
-const btnHold = document.querySelector(".btn--hold");
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 let scores, currentScore, activePlayer, playing;
 
@@ -28,11 +29,12 @@ const init = function () {
   current0El.textContent = 0;
   current1El.textContent = 0;
 
-  diceEl.classList.add("hidden"); //hiding the dice that is already displayed
-  player0El.classList.remove("player--winner");
-  player1El.classList.remove("player--winner");
-  player0El.classList.add("player--active");
-  player1El.classList.remove("player--active");
+  diceEl.classList.add('hidden'); //hiding the dice that is already displayed
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  winnerInformation.classList.add('hidden');
 };
 
 // running the init function
@@ -43,26 +45,25 @@ const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0; // set the previous player current score to zero
   activePlayer = activePlayer === 0 ? 1 : 0; // switch to other player
   currentScore = 0; // set the curret score of the switched player to zero
-  player0El.classList.toggle("player--active"); // Toggle Actively removes the class if it there, and if it is not there it adds it...
-  player1El.classList.toggle("player--active");
+  player0El.classList.toggle('player--active'); // Toggle Actively removes the class if it there, and if it is not there it adds it...
+  player1El.classList.toggle('player--active');
 };
 
 //Rolling Dice functionality
 
-btnRoll.addEventListener("click", function () {
+btnRoll.addEventListener('click', function () {
   if (playing) {
     //1. Generating a random Number
     const dice = Math.floor(Math.random() * 6) + 1;
     //2. Display the Dice
-    diceEl.classList.remove("hidden"); // first remove the hidden class
+    diceEl.classList.remove('hidden'); // first remove the hidden class
     diceEl.src = `dice-${dice}.png`; //using the tempalte literate match the generate dice with the image
     //3. Check for the rolled 1; otherwise add the dice roll to the current score
     if (dice !== 1) {
       //add the dice to the current score; you should have defined the global variable for the score outside this function
       currentScore += dice; //the same as (currentScore = currentScore + 1)
 
-      document.getElementById(`current--${activePlayer}`).textContent =
-        currentScore;
+      document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
       //Switching to the next player
       //first know the current player for the current player, you need a global variable for that
@@ -71,22 +72,20 @@ btnRoll.addEventListener("click", function () {
   }
 });
 
-btnHold.addEventListener("click", function () {
+btnHold.addEventListener('click', function () {
   if (playing) {
     //1: add the current Score to the score of the active player's score
     scores[activePlayer] += currentScore; // scores[1] = scores [1] + currentScore
-    document.getElementById(`score--${activePlayer}`).textContent =
-      scores[activePlayer];
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
     //2: Check the score if it is >= 100
     if (scores[activePlayer] >= 20) {
       playing = false;
-      diceEl.classList.add("hidden");
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.add("player--winner");
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.remove("player--active");
+      diceEl.classList.add('hidden');
+      document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+      document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+
+      winnerInformation.classList.remove('hidden');
+      winnerInformation.textContent = ` Congratulations 🎉Player ${activePlayer + 1} Wins the Game 🥇`;
     } else {
       //Switch to the next player
       switchPlayer();
@@ -96,4 +95,4 @@ btnHold.addEventListener("click", function () {
 
 //Reseting the game using the Button Click of New Game
 
-btnNew.addEventListener("click", init);
+btnNew.addEventListener('click', init);
